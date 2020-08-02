b8d4bc5bd8ffa708307eaeeb8687a0651d97e5ae
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-08-01 19:28:29 -0500

    add renderUI for select elements

--- server.R
+++ server.R
@@ -1,5 +1,18 @@
 library(shiny)
 
+dataset_path <- "datasets/datos_ventas_libreria.csv"
+stores = sort(unique(data$Tienda))
+categories = sort(unique(data$Categoria))
+
 shinyServer(function(input, output) {
+  data <- reactive({read.csv(dataset_path)})
+  
+  output$selectStores <- renderUI({
+    selectInput("selectStores", "Seleccione una Tienda:",choices=stores, selected=1)
+  })
+  
+  output$selectCategories <- renderUI({
+    selectInput("selectCategories", "Seleccione una categoria:",choices=categories, selected=1)
+  })
   
 })
